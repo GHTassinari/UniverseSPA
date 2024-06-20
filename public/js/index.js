69488957ddde9b053ddb7981338f225b1e60b132
Guilherme Tassinari <102005103+GHTassinari@users.noreply.github.com>
2024-06-20 02:21:08 -0300

    updating an writting error in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ import { highlightCurrentPage } from "./highlightsHyperlink.js";
 
 const router = new Router();
 router.add("/", "./pages/home.html");
-router.add("/universe", "/.pages/universe.html");
+router.add("/universe", "./pages/universe.html");
 router.add("/exploration", "./pages/exploration.html");
 router.add(404, "./pages/404.html");
 
